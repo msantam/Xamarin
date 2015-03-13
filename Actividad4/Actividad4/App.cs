4c373e481a0a1980462900c70b8d0b2405b765b6
Miguel Santamaría <msantam@comunidad.unam.mx>
2015-03-12 22:50:15 -0600

    Actividad 4 para su revisión

--- Actividad4/Actividad4/App.cs
+++ Actividad4/Actividad4/App.cs
@@ -7,26 +7,13 @@ namespace Actividad4
 	{
 		public static Page GetMainPage ()
 		{	
-			//Se crea un objeto etiqueta
-			Label label = new Label();
-
-			//Se modifican sus propiedades
-			//Texto
-			label.Text = "Cámbiame";
-			//Color del texto
-			label.TextColor = Color.Blue;
-			//Alineación vertical (YAlign). Para la alineación horizontal, usar XAlign
-			label.YAlign = TextAlignment.Center;
-
-
-			//El mismo código usando inicializador de objetos
-			//Label label = new Label{ Text = "Cámbiame", TextColor =  Color.Blue, YAlign = TextAlignment.Center };
-
-			//Se crea una página y se le asigna como contenido la eqtiqueta que se creó
-			ContentPage contentPage = new ContentPage();
-			contentPage.Content = label;
-
-			return contentPage;
+			return new ContentPage {
+				Content = new Label {
+					Text = "Miguel Angel Santamaría Rosas - 088327817",
+					HorizontalOptions = LayoutOptions.Center,
+					TextColor = Color.Blue
+				},
+			};
 		}
 	}
 }
