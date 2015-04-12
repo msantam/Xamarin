866486cd3b7c3c7372d0efbc294cb0eb32c11983
Miguel Angel Santamaria Rosas <msantam@comunidad.unam.mx>
2015-04-12 13:11:49 -0500

    Actividad 7 de la unidad 1

---
+++ Actividad7/Actividad7/Pagina3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Actividad7
+{
+	public class Pagina3
+	{
+		public Pagina3 ()
+		{
+			return new ContentPage {
+				Content = new Label {
+					Text = "Página 3",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				},
+			};
+		}
+	}
+}
+
