f60ba421d6e02c80c349d6300bc9c24a464476a9
Miguel Santamaría <msantam@comunidad.unam.mx>
2015-04-15 22:01:17 -0500

    Creación de archivo Pagina 3 para actividad 7

---
+++ Actividad7/Actividad7/Pagina3.cs
@@ -0,0 +1,15 @@
+using System;
+using Xamarin.Forms;
+namespace Actividad7
+{
+	public class Pagina3
+	{
+		public Pagina3 ()
+		{
+			Label texto = new Label {
+				Text = "Página 3",
+				TextColor = Color.Aqua
+			};
+		}
+	}
+}
